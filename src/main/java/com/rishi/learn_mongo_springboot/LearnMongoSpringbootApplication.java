b763d520629cac451fe7b12c051b3767490a44a8
rishighai97 <rishighai97@gmail.com>
2024-02-01 20:39:43 +0530

    first commit

---
+++ src/main/java/com/rishi/learn_mongo_springboot/LearnMongoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.rishi.learn_mongo_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnMongoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnMongoSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rishi/learn_mongo_springboot/LearnMongoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishi.learn_mongo_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnMongoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
